73960152fdb26802fa4140184172f95cfcfbce6c
Mitchell Stirmel <MStirmel02@Gmail.com>
2023-11-11 21:02:21 -0600

    User validation

--- FinalBackend.Services/Services/UserService.cs
+++ FinalBackend.Services/Services/UserService.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
@@ -24,7 +26,36 @@ namespace FinalBackend.Services.Services
 
         public bool UserValidation(UserModel user)
         {
-            throw new NotImplementedException();
+
+            SqlConnection conn = new SqlConnection(_configuration["ConnectionStrings:Database"]);
+            var cmd = new SqlCommand("sp_validate_user", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.Add("@UserID", SqlDbType.NVarChar);
+            cmd.Parameters.Add("@PasswordHash", SqlDbType.NVarChar);
+
+            cmd.Parameters["@UserID"].Value = user.UserId;
+            cmd.Parameters["@PasswordHash"].Value = user.PasswordHash;
+
+            try
+            {
+                conn.Open();
+
+                var exec = cmd.ExecuteScalar();
+                if (exec.Equals(1))
+                {
+                    return true;
+                }
+                
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+
+            return true;
         }
     }
 }
