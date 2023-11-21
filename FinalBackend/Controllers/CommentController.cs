dbe68d17be21064023d728f97f2b6470a09d7b9c
Mitchell Stirmel <MStirmel02@Gmail.com>
2023-11-20 19:30:45 -0600

    Added service and controller for Comment functionality

---
+++ FinalBackend/Controllers/CommentController.cs
@@ -0,0 +1,27 @@
+﻿using FinalBackend.Services.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinalBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]/")]
+    public class CommentController
+    {
+        ICommentService _commentService;
+
+        public CommentController(ICommentService commentService)
+        {
+            _commentService = commentService;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public bool PostCommentController([FromBody][Required] string placeholder )
+        {
+            return true;
+        }
+
+
+    }
+}
